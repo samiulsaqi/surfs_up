eea869db2facf8950b4435f2f802c12d561cfd7f
Sami <samiulsaqi1@gmail.com>
2020-12-13 08:08:53 -0500

    Added Flask file

---
+++ app.py
@@ -0,0 +1,36 @@
+#import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#create engine, classes and session
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+#create app instance
+app = Flask(__name__)
+
+
+#create routes
+@app.route('/') #root route
+
+# set up additional routes
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
